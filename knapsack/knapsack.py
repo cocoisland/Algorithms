8be9949d04312042b19f9d5e8223dc55768fa87f
tony <ttony10101@gmail.com>
2021-01-15 17:12:48 -0800

    Update knapsack.py

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -38,6 +38,49 @@ be taking and outputs the combination with the best value.
 #   answer = knapsack_rec(items, capacity)
 #   return {'Value': answer[0], 'Chosen': sorted(list(answer[1]))}
 
+"""
+-----------------------------------------------------------------------------------------
+knapsack_solver answer is wrong. Try the following data for max value return
+val = [4,7,10] 
+wt = [3,3,5] 
+W =7
+n = len(val) 
+print(knapSack(W, wt, val, n))
+-------------------------------------
+This following program works.
+# A naive recursive implementation of 0-1 Knapsack Problem 
+  
+# Returns the maximum value that can be put in a knapsack of 
+# capacity W 
+def knapSack(W, wt, val, n): 
+  
+    # Base Case 
+    if n == 0 or W == 0 : 
+        return 0
+  
+    # If weight of the nth item is more than Knapsack of capacity 
+    # W, then this item cannot be included in the optimal solution 
+    if (wt[n-1] > W): 
+        return knapSack(W, wt, val, n-1) 
+  
+    # return the maximum of two cases: 
+    # (1) nth item included 
+    # (2) not included 
+        else: 
+        return max(val[n-1] + knapSack(W-wt[n-1], wt, val, n-1), 
+                   knapSack(W, wt, val, n-1)) 
+  
+# end of function knapSack 
+  
+# To test above function 
+val = [60, 100, 120] 
+wt = [10, 20, 30] 
+W = 50
+n = len(val) 
+print knapSack(W, wt, val, n) 
+
+------------------------------------------------------------------------------------
+"""
 
 
 # Memoized version of our brute-force solution
