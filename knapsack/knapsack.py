b7e11e775e9e6b4438e425daf39c8b698c146bc2
tony <ttony10101@gmail.com>
2021-01-17 13:24:02 -0800

    Update knapsack.py

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -124,7 +124,39 @@ def knapsack_solver(items, capacity):
   answer = knapsack_bf_helper(len(items) - 1, capacity)
   return {'Value': answer[0], 'Chosen': sorted(list(answer[1]))}
 
+'''
+# Bottom up memoization
+# K table tracking value at each incremental Weight for each weight denominations
+# val = [60, 100, 120] 
+# wt = [10, 20, 30] 
+# W = 50
+# n = len(val) 
+def knapSack(W, wt, val, n): 
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)] 
+  
+    # Build table K[][] in bottom up manner 
+    for i in range(n + 1): 
+        for w in range(W + 1): 
+            if i == 0 or w == 0: 
+                K[i][w] = 0
+            elif wt[i-1] <= w: 
+                K[i][w] = max(val[i-1] 
+                          + K[i-1][w-wt[i-1]],   
+                              K[i-1][w]) 
+            else: 
+                K[i][w] = K[i-1][w] 
+  
+    return K[n][W] 
+  
+  
+# Driver code 
+val = [60, 100, 120] 
+wt = [10, 20, 30] 
+W = 50
+n = len(val) 
+print(knapSack(W, wt, val, n)) 
 
+'''
 """
 Incorrect but feasible and efficient solution for the knapsack problem. due to sorting weight by value
 """
