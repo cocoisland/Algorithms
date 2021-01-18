330e8316db86fe348ae5de470c9e430fa8d962d5
tony <ttony10101@gmail.com>
2021-01-17 22:54:02 -0800

    Update recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -7,13 +7,12 @@ return 0 if no ingredients.
 import math
 
 def recipe_batches(recipe, ingredients):
-  recipeKeys = recipe.keys()
   maxBatch = 0
   batchCount = 0
 
-  for k in recipeKeys:
+  for k in recipe.keys():
     if k in ingredients:
-      batchCount = ingredients[k] // recipe[k]
+      batchCount = recipe[k] // ingredients[k]
       
       if batchCount > maxBatch:
         maxBatch = batchCount
@@ -21,11 +20,12 @@ def recipe_batches(recipe, ingredients):
       return 0
   
 
-  return batches
+  return maxBatch
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+  ingredients = { 'milk': 100, 'butter': 50, 'flour': 5 }
+  recipe = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  maxBatch=recipe_batches(recipe, ingredients)
+  print(f'{maxBatch} batches can be made from the available ingredients: {ingredients}')
