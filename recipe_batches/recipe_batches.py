b3d17b2619cf1612893d567ea9e03d9c3322bb52
tony <ttony10101@gmail.com>
2021-01-17 23:07:51 -0800

    Update recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 '''
 Plan:
-find the smallest number of recipe that can fit in available ingredients.
+find the smallest number of recipe that can be made from ingredients.
 return 0 if no ingredients.
 '''
 import math
@@ -10,9 +10,9 @@ def recipe_batches(recipe, ingredients):
   maxBatch = 0
   batchCount = 0
 
-  for k in recipe.keys():
-    if k in ingredients:
-      batchCount = recipe[k] // ingredients[k]
+  for k in ingredients.keys():
+    if k in recipe:
+      batchCount = ingredients[k] // recipe[k]
       
       if batchCount > maxBatch:
         maxBatch = batchCount
@@ -25,7 +25,7 @@ def recipe_batches(recipe, ingredients):
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  ingredients = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  recipe = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
   maxBatch=recipe_batches(recipe, ingredients)
   print(f'{maxBatch} batches can be made from the available ingredients: {ingredients}')
